98c432476abe1c5703945cb8c0c3e70084d10ab7
Rosa Pires <rosapires@laptop-3ilr8c5d.kea.local>
2018-05-18 13:11:01 +0200

    donedone

---
+++ w20thursday/materials/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// listen for clicks on the color-selectors
+
+// when one is clicked:
+
+    // find the element that was clicked
+
+    // find the color from that element
+
+    // set the css variable --basecolor 
