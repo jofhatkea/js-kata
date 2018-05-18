e1121fdb3f91a0694b75f94ba8931b99e9df2681
Rosa Pires <rosapires@laptop-3ilr8c5d.kea.local>
2018-05-18 13:13:28 +0200

    donedone

--- w20thursday/materials/script.js
+++ w20thursday/materials/script.js
@@ -2,6 +2,14 @@
 
 // listen for clicks on the color-selectors
 
+let container = document.querySelector("#colorselector");
+container.addEventListener("click", e=>{
+    //console.log(e.target)
+    const clickedLi = e.target;
+    console.log(clickedLi.dataset.color);
+    document.documentElement.style.setProperty("--basecolor", clickedLi.dataset.color);
+})
+
 // when one is clicked:
 
     // find the element that was clicked
